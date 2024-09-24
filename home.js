cde840e400aba4fdd421b5385f7ba99a1941759f
Hasibul Hasan <hasibul277127@gmail.com>
2024-09-24 16:33:18 +0600

    added all the functionality

--- home.js
+++ home.js
@@ -1,15 +1,36 @@
+// display toggle functions 
+document.getElementById('donation-button')
+.addEventListener('click',function(event){
+    event.preventDefault();
+    showActiveButton('donation-section');
+
+    document.getElementById('donation-button').classList.remove('bg-btnTertiary','border-2','border-gray-200');
+    document.getElementById('donation-button').classList.add('bg-btnPrimary');
+
+    document.getElementById('history-button').classList.add('bg-btnTertiary','border-2','border-gray-200');
+    document.getElementById('history-button').classList.remove('bg-btnPrimary');
+})
+document.getElementById('history-button')
+.addEventListener('click',function(event){
+    event.preventDefault();
+    showActiveButton('history-section');
+
+    document.getElementById('history-button').classList.remove('bg-btnTertiary','border-2','border-gray-200');
+    document.getElementById('history-button').classList.add('bg-btnPrimary');
+
+    document.getElementById('donation-button').classList.add('bg-btnTertiary','border-2','border-gray-200');
+    document.getElementById('donation-button').classList.remove('bg-btnPrimary');
+})
+
+
+
+// donation operation functions 
 document.getElementById('donate-input-button')
 .addEventListener('click',function(event){
     event.preventDefault();
-    const donateInputValue=getInputValue('donate-input-value');
-    const currentBalance=getTextValue('current-value');
-    if(isNaN(donateInputValue)||donateInputValue>currentBalance)
-    {
-        alert('Transaction error');
-        return;
-    }
-    my_modal_5.showModal();
-    // const newBalance=
-    // const newDonateBalance=
+    // clickFunction(donateInputValue,donatedBalance,eventName)
+    clickFunction('donate-input-value','donated-balance','Flood at Noakhali, Bangladesh');    
 
 })
+
+
--- utility.js
+++ utility.js
@@ -1,3 +1,4 @@
+// converting sting values of input and balance 
 function getInputValue(id){
     const inputValue= document.getElementById(id).value;
     for(let i=0;i<inputValue.length;i++)
@@ -14,4 +15,59 @@ function getTextValue(id){
     const textValue= document.getElementById(id).innerText;
     const floatValue =parseFloat(textValue);
     return floatValue;
+}
+
+
+// adding donation in history section 
+function getHistory(inputValue,eventName)
+{
+    const historySection=document.getElementById('history-section');
+    const parentDiv=document.createElement('div');
+    parentDiv.classList.add('border-2','rounded','p-5','space-y-4')
+
+    const h2=document.createElement('h2');
+    h2.classList.add('font-bold')
+    h2.innerText=`${inputValue} taka is donated  for ${eventName}`;
+
+    const p=document.createElement('p');
+    const now=new Date()
+    p.innerText=`${now}`;
+
+    parentDiv.appendChild(h2)
+    parentDiv.appendChild(p)
+    historySection.appendChild(parentDiv);
+}
+
+
+// donate onclick operation
+function clickFunction(donateInput,donateMoney,eventName){
+    const donateInputValue=getInputValue(donateInput);
+    const currentBalance=getTextValue('current-balance');
+    const donatedBalance=getTextValue(donateMoney)
+    if(isNaN(donateInputValue)||donateInputValue>currentBalance)
+    {
+        alert('Transaction error \nPlease try again');
+        return;
+    }
+    my_modal_5.showModal();
+
+    const newBalance=currentBalance-donateInputValue;
+    document.getElementById('current-balance').innerText=newBalance;
+
+    const newDonatedBalance=donatedBalance+donateInputValue;
+    document.getElementById(donateMoney).innerText=newDonatedBalance;
+    
+    getHistory(donateInputValue,eventName);
+}
+
+
+// toggling the active button 
+
+function showActiveButton(id){
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+
+    
 }
